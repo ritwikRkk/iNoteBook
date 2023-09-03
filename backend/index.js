cabaef9091dec43821d9b2e9c458f3b9575ec97b
“rohitRKK” <myfantasylearningskills@gmail.com>
2023-09-03 09:38:56 +0530

    completed

---
+++ backend/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const connectToMongo = require('./db');
+const PORT = process.env.PORT || 5000;
+const cors = require('cors');
+const app = express();
+require('dotenv').config()
+
+
+app.use(express.json());
+app.use(cors());
+
+connectToMongo();
+
+// Available Routes
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/notes', require('./routes/notes'));
+
+
+app.get('/', (req, res) => {
+    res.send("Hello world!");
+});
+
+app.listen(PORT, ()=>{
+    console.log(`iNotebook Server listening on port ${PORT}`);
+});
---
+++ client/src/components/Login.js
@@ -0,0 +1,57 @@
+import React, { useState, useContext } from 'react'
+import { useNavigate } from 'react-router-dom';
+import alertContext from '../context/alert/alertContext';
+
+const Login = () => {
+
+    const context = useContext(alertContext);
+    const { showAlert } = context;
+
+    const [credentials, setCredentials] = useState({ email: "", password: "" });
+    let navigate = useNavigate();
+
+    const onChange = (event) => {
+        const { name, value } = event.target;
+        setCredentials({ ...credentials, [name]: value })
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        const response = await fetch("http://localhost:5000/api/auth/login", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ email: credentials.email, password: credentials.password })
+        });
+        const json = await response.json();
+        if (json.success) {
+            // Save the Auth token and redirect
+            localStorage.setItem("auth-token", json.authToken);
+            navigate("/");
+            showAlert("success", "LoggedIn Successfully.");
+        } else{
+            showAlert("danger", "some error occured.");
+          }
+        //   console.log(json);
+    };
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <div className="mb-3">
+                    <label htmlFor="email" className="form-label">Email address</label>
+                    <input type="email" value={credentials.email} className="form-control" id="email" name="email" aria-describedby="emailHelp" onChange={onChange} />
+                    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="password" className="form-label">Password</label>
+                    <input type="password" value={credentials.password} name="password" className="form-control" id="password" onChange={onChange} />
+                </div>
+                <button type="submit" className="btn btn-primary" >Submit</button>
+            </form>
+        </div>
+    )
+}
+
+export default Login
